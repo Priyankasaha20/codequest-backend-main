1d0b408adbbaf13dc7b8ddacb8115b6475401a6d
AriaFantom <priyanshudebnath348@gmail.com>
2025-07-02 08:36:10 +0530

    Remove unused controllers, models, and routes for AI stats, interview types, sessions, and AI service

--- src/controllers/interviewTypesController.js
+++
@@ -1,8 +0,0 @@
-export async function listInterviewTypes(req, res, next) {
-  try {
-    const types = await InterviewType.find({});
-    res.json(types);
-  } catch (err) {
-    next(err);
-  }
-}
--- src/routes/interviewTypes.js
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import { listInterviewTypes } from "../controllers/interviewTypesController.js";
-
-const router = Router();
-router.get("/",  listInterviewTypes);
-export default router;
